68e690ded70fd1a00a8110d18bd3b7a1447b3bf1
skullzado <pafcalzado@gmail.com>
2025-06-02 20:31:11 +0800

    Add LeafNode class, add unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,7 +17,6 @@ class HTMLNode:
                 inner_dict = 'style="'
                 for i_key, i_value in value.items():
                     inner_dict += f"{i_key}: {i_value}; "
-                print(inner_dict)
                 props_str += inner_dict[:-2].strip() + '"'
             else:
                 props_str += f'{key}="{value}" '
@@ -25,3 +24,18 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
